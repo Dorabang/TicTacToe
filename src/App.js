bf609bec1b2405207d3bfa73250455b5776acecc
Dorabang <psa1195@naver.com>
2023-06-03 14:44:53 +0900

    chore: install create-react-app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className='game'>
+      <div className='game-board'></div>
+      <div className='game-info'>
+        <div></div>
+        <ul></ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
